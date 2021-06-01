bc36e4abd81f68559be5dfb93ff8381613975526
mordehaibenharush <63501119+mordehaibenharush@users.noreply.github.com>
2021-06-01 21:39:40 +0300

    http post request

--- flutterfire_samples/lib/screens/join_queue_screen.dart
+++ flutterfire_samples/lib/screens/join_queue_screen.dart
@@ -3,10 +3,14 @@ import 'queue_status_screen.dart';
 import 'package:flutterfire_samples/res/custom_colors.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class JoinQueueScreen extends StatelessWidget {
   final User user;
   final CollectionReference queue1 = FirebaseFirestore.instance.collection('queue1');
+  final FirebaseMessaging _fcm = FirebaseMessaging.instance;
   JoinQueueScreen({required this.user});
 
   @override
@@ -37,11 +41,22 @@ class JoinQueueScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ElevatedButton(onPressed: () {
-                queue1.doc(user.uid).set({
-                'timestamp': Timestamp.now(),
-                'user':  user.displayName! + "_" + user.uid
-              });
+              ElevatedButton(onPressed: () async {
+                String? fcmToken = await _fcm.getToken();
+                http.Response response = await http.post(
+                Uri.parse('https://us-central1-iotrain-49a8d.cloudfunctions.net/api/add_user'),
+                body: {
+                  'device_id': '1',
+                  'user': user.displayName! + "_" + user.uid,
+                  'fcm_token': fcmToken
+                },
+                );
+                var jsonResponse = jsonDecode(response.body);
+                print(jsonResponse);
+
+                // queue1.doc(user.uid).set({
+                // 'timestamp': Timestamp.now(),
+                // 'user':  user.displayName! + "_" + user.uid});
             },
             style: ElevatedButton.styleFrom(primary: Colors.orange),
             child: Text("Join Machine 1 queue", style: TextStyle(color: Colors.black, fontSize: 20))),
--- flutterfire_samples/lib/screens/sign_in_screen.dart
+++ flutterfire_samples/lib/screens/sign_in_screen.dart
@@ -4,6 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:flutterfire_samples/res/custom_colors.dart';
 import 'package:flutterfire_samples/screens/user_info_screen.dart';
 import 'package:flutterfire_samples/widgets/sign_in_form.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutterfire_samples/utils/notification.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("Handling a background message: ${message.messageId}");
+}
 
 class SignInScreen extends StatefulWidget {
   @override
@@ -13,6 +19,92 @@ class SignInScreen extends StatefulWidget {
 class _SignInScreenState extends State<SignInScreen> {
   final FocusNode _emailFocusNode = FocusNode();
   final FocusNode _passwordFocusNode = FocusNode();
+  late final FirebaseMessaging _messaging;
+  // late int _totalNotifications;
+  // PushNotification? _notificationInfo;
+
+  // void registerNotification() async {
+  //   await Firebase.initializeApp();
+  //   _messaging = FirebaseMessaging.instance;
+
+  //   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  //   NotificationSettings settings = await _messaging.requestPermission(
+  //     alert: true,
+  //     badge: true,
+  //     provisional: false,
+  //     sound: true,
+  //   );
+
+  //   if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+  //     print('User granted permission');
+
+  //     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+  //       print(
+  //           'Message title: ${message.notification?.title}, body: ${message.notification?.body}, data: ${message.data}');
+
+  //       // Parse the message received
+  //       PushNotification notification = PushNotification(
+  //         title: message.notification?.title,
+  //         body: message.notification?.body,
+  //         dataTitle: message.data['title'],
+  //         dataBody: message.data['body'],
+  //       );
+
+  //       setState(() {
+  //         _notificationInfo = notification;
+  //         _totalNotifications++;
+  //       });
+  //     });
+  //   } else {
+  //     print('User declined or has not accepted permission');
+  //   }
+  // }
+
+  // // For handling notification when the app is in terminated state
+  // checkForInitialMessage() async {
+  //   await Firebase.initializeApp();
+  //   RemoteMessage? initialMessage =
+  //       await FirebaseMessaging.instance.getInitialMessage();
+
+  //   if (initialMessage != null) {
+  //     PushNotification notification = PushNotification(
+  //       title: initialMessage.notification?.title,
+  //       body: initialMessage.notification?.body,
+  //       dataTitle: initialMessage.data['title'],
+  //       dataBody: initialMessage.data['body'],
+  //     );
+
+  //     setState(() {
+  //       _notificationInfo = notification;
+  //       _totalNotifications++;
+  //     });
+  //   }
+  // }
+
+  // @override
+  // void initState() {
+  //   _totalNotifications = 0;
+  //   registerNotification();
+  //   checkForInitialMessage();
+
+  //   // For handling notification when the app is in background
+  //   // but not terminated
+  //   FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+  //     PushNotification notification = PushNotification(
+  //       title: message.notification?.title,
+  //       body: message.notification?.body,
+  //       dataTitle: message.data['title'],
+  //       dataBody: message.data['body'],
+  //     );
+
+  //     setState(() {
+  //       _notificationInfo = notification;
+  //       _totalNotifications++;
+  //     });
+  //   });
+
+  //   super.initState();
+  // }
 
   Future<FirebaseApp> _initializeFirebase() async {
     FirebaseApp firebaseApp = await Firebase.initializeApp();
