0c3ec6936ee730768349e0e93d06b69a1a414dfb
mordehaibenharush <63501119+mordehaibenharush@users.noreply.github.com>
2021-06-08 18:30:57 +0300

    messaging not working

--- flutterfire_samples/lib/main.dart
+++ flutterfire_samples/lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutterfire_samples/screens/sign_in_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+
+
+  Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("Handling a background message: ${message.messageId}");
+}
 
 void main() {
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+  print('Got a message whilst in the foreground!');
+  print('Message data: ${message.data}');
+  });
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(MyApp());
 }
 
